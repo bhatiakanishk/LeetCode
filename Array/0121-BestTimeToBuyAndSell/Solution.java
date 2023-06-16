2d55f05eceb3de1e7a1700bbd19126e3f31900bd
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-06-15 19:33:47 -0700

    Create Solution.java

---
+++ Array/0121-BestTimeToBuyAndSell/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
+
+/*
+ * Space Complexity: O(1)
+ * Time Complexity: O(n)
+ */
