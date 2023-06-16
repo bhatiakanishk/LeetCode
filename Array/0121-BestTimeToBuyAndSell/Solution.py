c431bb23be5db1af65a934f455a2f711116fe8e3
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-06-15 19:42:25 -0700

    Create Solution.py

---
+++ Array/0121-BestTimeToBuyAndSell/Solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = float('inf')
+        maxProfit = 0
+        
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
+            elif price - minPrice > maxProfit:
+                maxProfit = price - minPrice
+        
+        return maxProfit
+
+"""
+Space Complexity: O(1)
+Time Complexity: O(n)
+"""
