5bf508b5fc54272ed6e430613ae3b3ed4a82514e
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-08-14 19:07:36 -0400

    Update Solution.py

--- Array/0238-ProductOfArray/Solution.py
+++ Array/0238-ProductOfArray/Solution.py
@@ -2,17 +2,16 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        result = [1] * n
-        right_prod = 1
-        # Calculate the product of all elements to the left of each element
-        for i in range(1, n):
-            result[i] = result[i-1] * nums[i-1]
-        # Calculate the product of all elements to the right of each element and multiply with the left product
-        for i in range(n-1, -1, -1):
-            result[i] *= right_prod
-            right_prod *= nums[i]
-        return result
+        length = len(nums)
+        sol = [1] * length
+        pre = 1
+        post = 1
+        for i in range(length):
+            sol[i] *= pre
+            pre = pre * nums[i]
+            sol[length - i - 1] *= post
+            post = post * nums[length - i - 1]
+        return(sol)
 
 """
 Space Complexity: O(1)
