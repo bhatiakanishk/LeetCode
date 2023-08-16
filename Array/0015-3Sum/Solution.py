4ebc8f3a37bd5a834096100046be9d1662d8b416
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-08-16 15:34:55 -0400

    Create Solution.py

---
+++ Array/0015-3Sum/Solution.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # Sort the array in ascending order
+        result = []
+        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # Skip duplicate elements to avoid duplicate triplets
+            
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        return result
+
+"""
+Space Complexity: O(1)
+Time Complexity: O(n ^ 2)
+"""
