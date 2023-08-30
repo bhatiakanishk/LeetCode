2562165f902bd00fc2a19a2868774594ab05931b
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-08-29 17:24:12 -0700

    Create Solution.py

---
+++ Binary/0338-CountingBits/Solution.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+"""
