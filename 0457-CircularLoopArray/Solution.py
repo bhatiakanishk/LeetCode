03bc26ef7bdc60f35c0eb6db39dd2527a6b011aa
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-09-29 16:18:30 -0700

    Create Solution.py

---
+++ 0457-CircularLoopArray/Solution.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        n, visited = len(nums), set()
+        for i in range(n):
+            if i not in visited:
+                local_s = set()
+                while True:
+                    if i in local_s: return True
+                    if i in visited: break         
+                    visited.add(i)
+                    local_s.add(i)
+                    prev, i = i, (i + nums[i]) % n
+                    if prev == i or (nums[i] > 0) != (nums[prev] > 0): break
+        return False
+
+"""
+Time Complexity: O(n^2)
+Space Complexity: O(n)
+"""
