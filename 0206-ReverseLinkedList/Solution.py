8a2527ff7a22a602a13bb472871fc188386c74be
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-11-29 14:00:41 -0800

    Create Solution.py

---
+++ 0206-ReverseLinkedList/Solution.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev_node = None
+        current_node = head
+        
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        
+        return prev_node
+
+"""
+Space Complexity: O(1)
+Time Complexity: O(n)
+"""
