d5bcb8b804371ae9844d8fbe2e1e440816d8b81c
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-12-04 18:29:27 -0800

    Create Solution.py

---
+++ 0026-RemoveDuplicatesFromSortedArray/Solution.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+        return j + 1
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
