0299b071b5ced2299fa42dcc96d55d111870fc96
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-12-21 17:56:23 -0800

    Create Solution.py

---
+++ 0066-PlusOne/Solution.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        return [1] + digits
+
+"""
+Space Complexity: O(1)
+Time Complexity: O(n)
+"""
