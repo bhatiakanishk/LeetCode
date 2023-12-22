849d095a7abdcbd8c2dff29f427e0c1605c9dee6
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-12-21 17:49:10 -0800

    Create Solution.py

---
+++ 0058-LengthOfLastWord/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        length = 0
+        i = len(s) - 1
+        
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+        return length
+
+"""
+Space Complexity: O(1)
+Time Complexity: O(n)
+"""
