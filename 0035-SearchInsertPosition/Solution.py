f4f2c93f037dccd5419cab28fa59384bbb61efd5
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-12-21 17:41:03 -0800

    Create Solution.py

---
+++ 0035-SearchInsertPosition/Solution.py
@@ -0,0 +1,18 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
+"""
+Space Complexity: O(1)
+Time Complexity: O(n log n)
+"""
